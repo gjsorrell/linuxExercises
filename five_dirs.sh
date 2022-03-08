5b7176cdeba5796d2df0236f502a4cbf81f402f2
gjsorrell <gregorysorrell@gmail.com>
2022-03-07 22:28:29 -0600

    Completed five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+mkdir five
+dir="five/dir"
+i=1
+x=7;
+while [[ $i -le 5 ]];
+do
+	dir_num=$dir$i;
+	mkdir $dir_num;
+	j=1;
+	while [[ $j -le 5 ]];
+	do
+		dir_num_file=$dir_num/file$j;
+		touch $dir_num_file;
+		k=1;
+		while [[ $k -le $j ]];
+		do
+			echo $j >> $dir_num_file;
+			k=$((k+1));	
+		done;
+		j=$((j+1));	
+	done;		
+	i=$((i+1));
+done
+
+
+	    
